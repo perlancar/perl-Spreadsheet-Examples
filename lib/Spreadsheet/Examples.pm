c96221f791859d7d3d328e18d63bb07c69aaf861
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-11-09 10:14:49 +0700

    Initial commit

---
+++ lib/Spreadsheet/Examples.pm
@@ -0,0 +1,37 @@
+## no critic: TestingAndDebugging::RequireUseStrict
+package Spreadsheet::Examples;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A collection of various examples of spreadsheet files
+
+=head1 DESCRIPTION
+
+This distribution contains, in its share directory, a collection of various
+spreadsheet files, usually for testing or benchmarking purposes:
+
+=over
+
+=item * 10sheet-10row-10col.{ods,xlsx}
+
+A workbook containing 10 sheets, each sheet containing 10 rows and 10 columns.
+The cells are numbers. Minimum formatting.
+
+=item * 10sheet-1000row-10col.{ods,xlsx}
+
+A workbook containing 10 sheets, each sheet containing 1000 rows and 10 columns.
+The cells are numbers. Minimum formatting.
+
+=item * 10sheet-empty.{ods,xlsx}
+
+Empty 10-sheet workbook.
+
+=item * empty.{ods,xlsx}
+
+=back
+
+=cut
